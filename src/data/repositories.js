38a797893b39cf42c8ccbe7ab5e2e08a30d0f385
Deiby Graciano <deibygj@hotmail.com>
2024-04-29 16:29:44 -0500

    primer deployd

--- src/data/repositories.js
+++ src/data/repositories.js
@@ -1,103 +1,103 @@
 const repositories = [
     {
         id: "1",
-        fullName: 'melasuda perez',
+        fullName: 'Carlos Perez',
         stars: '2'
     },
     {
         id: "2",
-        fullName: 'melasuda perez',
+        fullName: 'Carlos Perez',
         stars: '2'
     }
     ,
     {
         id: "3",
-        fullName: 'melasuda perez',
+        fullName: 'Carlos Perez',
         stars: '2'
     },
     {
         id: "4",
-        fullName: 'melasuda perez',
+        fullName: 'Carlos Perez',
         stars: '2'
     },
     {
         id: "5",
-        fullName: 'melasuda perez',
+        fullName: 'Carlos Perez',
         stars: '2'
     },
     {
         id: "6",
-        fullName: 'melasuda perez',
+        fullName: 'Carlos Perez',
         stars: '2'
     },
     {
         id: "7",
-        fullName: 'melasuda perez',
+        fullName: 'Carlos Perez',
         stars: '2'
     },
     {
         id: "8",
-        fullName: 'melasuda perez',
+        fullName: 'Carlos Perez',
         stars: '2'
     },
     {
         id: "9",
-        fullName: 'melasuda perez',
+        fullName: 'Carlos Perez',
         stars: '2'
     },
     {
         id: "10",
-        fullName: 'melasuda perez',
+        fullName: 'Carlos Perez',
         stars: '2'
     },
     {
         id: "11",
-        fullName: 'melasuda perez',
+        fullName: 'Carlos Perez',
         stars: '2'
     },
     {
         id: "12",
-        fullName: 'melasuda perez',
+        fullName: 'Carlos Perez',
         stars: '2'
     },
     {
         id: "13",
-        fullName: 'melasuda perez',
+        fullName: 'Carlos Perez',
         stars: '2'
     },
     {
         id: "14",
-        fullName: 'melasuda perez',
+        fullName: 'Carlos Perez',
         stars: '2'
     },
     {
         id: "15",
-        fullName: 'melasuda perez',
+        fullName: 'Carlos Perez',
         stars: '2'
     },
     {
         id: "16",
-        fullName: 'melasuda perez',
+        fullName: 'Carlos Perez',
         stars: '2'
     },
     {
         id: "17",
-        fullName: 'melasuda perez',
+        fullName: 'Carlos Perez',
         stars: '2'
     },
     {
         id: "18",
-        fullName: 'melasuda perez',
+        fullName: 'Carlos Perez',
         stars: '2'
     },
     {
         id: "19",
-        fullName: 'melasuda perez',
+        fullName: 'Carlos Perez',
         stars: '2'
     },
     {
         id: "20",
-        fullName: 'melasuda perez',
+        fullName: 'Carlos Perez',
         stars: '2'
     }
 ];
